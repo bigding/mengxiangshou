151bdd5d4683ba51c1876598d96e517ef631e51f
bigding <tiandingdingding@gmail.com>
2017-02-10 23:04:54 +0800

    将顶部和底部单独分出来了

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!--Footer-part-->
+<div class="row-fluid">
+    <div id="footer" class="span12">萌萌不胖队@版权所有
+    </div>
+</div>
+<!--end-Footer-part-->
---
+++ header.php
@@ -0,0 +1,60 @@
+
+<!--Header-part-->
+<div id="header">
+    <h1><a href="dashboard.html">Matrix Admin</a></h1>
+</div>
+<!--close-Header-part-->
+
+<!--top-Header-menu-->
+<div id="user-nav" class="navbar navbar-inverse">
+    <ul class="nav">
+        <li class="dropdown" id="profile-messages"><a title="" href="#" data-toggle="dropdown"
+                                                      data-target="#profile-messages" class="dropdown-toggle"><i
+                    class="icon icon-user"></i> <span class="text">Welcome User</span><b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
+                <li class="divider"></li>
+                <li><a href="#"><i class="icon-check"></i> My Tasks</a></li>
+                <li class="divider"></li>
+                <li><a href="login.html"><i class="icon-key"></i> Log Out</a></li>
+            </ul>
+        </li>
+        <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages"
+                                                   class="dropdown-toggle"><i class="icon icon-envelope"></i> <span
+                    class="text">Messages</span> <span class="label label-important">5</span> <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a class="sAdd" title="" href="#"><i class="icon-plus"></i> new message</a></li>
+                <li class="divider"></li>
+                <li><a class="sInbox" title="" href="#"><i class="icon-envelope"></i> inbox</a></li>
+                <li class="divider"></li>
+                <li><a class="sOutbox" title="" href="#"><i class="icon-arrow-up"></i> outbox</a></li>
+                <li class="divider"></li>
+                <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> trash</a></li>
+            </ul>
+        </li>
+        <li class=""><a title="" href="#"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
+        <li class=""><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a>
+        </li>
+    </ul>
+</div>
+
+<!--start-top-serch-->
+<div id="search">
+    <input type="text" placeholder="Search here..."/>
+    <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
+</div>
+<!--close-top-serch-->
+
+<!--sidebar-menu-->
+
+<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-list"></i>Forms</a>
+    <ul>
+        <li><a href="#"><i class="icon icon-home"></i> <span>用户信息</span></a></li><!--姓名,密码,电话号码,邮件地址,身高,体重,性别,签名,生日,拥有宠物,金币数量,操作:查看具体的宠物和拥有的服装类型还有具体运动完成情况,历史体重,日常记录-->
+        <li><a href="#"><i class="icon icon-signal"></i> <span>宠物信息</span></a></li><!--宠物名称,宠物类别,宠物价值,宠物简介.操作:宠物详情,查看图片-->
+        <li><a href="#"><i class="icon icon-inbox"></i> <span>服装信息</span></a></li><!--服装名称,服装价值,服装简介,操作:查看详情,查看图片-->
+        <li><a href="#"><i class="icon icon-th"></i> <span>运动信息</span></a></li><!--运动名称,运动悬赏价值,运动简介,操作:查看详情-->
+        <li><a href="#"><i class="icon icon-fullscreen"></i> <span>饮食信息</span></a></li><!--饮食名称,饮食悬赏金额,饮食简介,操作:查看详情,查看图片-->
+        <li><a href="#"><i class="icon icon-list"></i> <span>读物信息</span> </a></li><!--读物名称,读物描述,读物悬赏金额,读物简介,操作:查看详情,查看图片-->
+        <li><a href="#"><i class="icon icon-tint"></i> <span>日推设计</span></a></li><!--设计日推饮食,运动,读物-->
+    </ul>
+</div>
